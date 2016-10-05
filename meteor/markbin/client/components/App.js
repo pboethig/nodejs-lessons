b07ac75f88520f2a520e935abf1b5838bdd21a21
pboethig <pboethig@gmail.com>
2016-10-05 08:53:40 +0200

    markbin: add component binslist

--- meteor/markbin/client/components/App.js
+++ meteor/markbin/client/components/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Header from './Header';
-
+import BinsList from './BinsList'
 
 export default ()=>
 {
-    return (<Header/>);
+    return (
+        <div>
+        <Header/>
+        <BinsList/>
+        </div>
+        );
 }
--- meteor/markbin/server/main.js
+++ meteor/markbin/server/main.js
@@ -1,6 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 import {Bins} from '../imports/collections/bins';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+Meteor.startup(() => 
+{
+  Meteor.publish('bins', function()
+  {
+    Bins.find({ownerId:this.userId});
+  });
+
 });
